20ed98608c640b0ca58fa458e149b2131815443c
prateekbose <34328350+prateekbose@users.noreply.github.com>
2020-08-30 20:46:55 +0530

    first commit

---
+++ assets/js/animate.js
@@ -0,0 +1,52 @@
+const navAnimate = () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+    const nav = document.querySelector('nav');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger-active');
+        navLinks.classList.toggle('nav-links-active');
+        nav.classList.toggle('nav-active');
+        dropDown();
+    });
+};
+
+const navScroll = () => {
+    const nav = document.querySelector('nav');
+    const menu = document.querySelector('.drop-down');
+    var menuClassList = [...menu.classList];
+    
+    document.addEventListener('scroll', () => {
+        if(!menuClassList.includes("drop-down-active")){
+            if(window.scrollY >= nav.offsetHeight){
+                nav.classList.add('nav-scroll');
+            } else {
+                nav.classList.remove('nav-scroll');
+            }
+        }
+        
+    });
+};
+
+const dropDown = () => {
+    const menu = document.querySelector('.drop-down');
+    const nav = document.querySelector('nav');
+    const body = document.body;
+    var navClassList = [...nav.classList];
+    var menuClassList = [...menu.classList];
+
+    if(navClassList.includes("nav-scroll")){
+        nav.classList.toggle('nav-scroll');
+    }
+    if(menuClassList.includes("drop-down-active")){
+        nav.classList.toggle('nav-scroll');
+    }
+    menu.classList.toggle('drop-down-active');
+    body.classList.toggle('disable-scroll');
+}
+
+const start = () => {
+    navAnimate();
+    navScroll();
+};
+
+start();
